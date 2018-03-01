f9d0f18add5da33766dd0feba3e4331bee56ee10
安藤亮介 <crawd4274@gmail.com>
2018-03-01 22:16:17 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from RPi import GPIO
+
+SPICLK, SPIMOSI, SPIMISO, SPICS=11,10,9,8
+
+# MCP3208からSPI通信で12ビットのデジタル値を取得。0から7の8チャンネル使用可
+def readadc(adcnum, clockpin, mosipin, misopin, cspin):
+    if adcnum > 7 or adcnum < 0:
+        return -1
+    GPIO.output(cspin, GPIO.HIGH)
+    GPIO.output(clockpin, GPIO.LOW)
+    GPIO.output(cspin, GPIO.LOW)
+
+    commandout = adcnum
+    commandout |= 0x18  # スタートビット＋シングルエンドビット
+    commandout <<= 3  # LSBから8ビット目を送信するようにする
+    for i in range(5):
+        # LSBから数えて8ビット目から4ビット目までを送信
+        if commandout & 0x80:
+            GPIO.output(mosipin, GPIO.HIGH)
+        else:
+            GPIO.output(mosipin, GPIO.LOW)
+        commandout <<= 1
+        GPIO.output(clockpin, GPIO.HIGH)
+        GPIO.output(clockpin, GPIO.LOW)
+    adcout = 0
+    # 13ビット読む（ヌルビット＋12ビットデータ）
+    for i in range(13):
+        GPIO.output(clockpin, GPIO.HIGH)
+        GPIO.output(clockpin, GPIO.LOW)
+        adcout <<= 1
+        if i > 0 and GPIO.input(misopin) == GPIO.HIGH:
+            adcout |= 0x1
+    GPIO.output(cspin, GPIO.HIGH)
+    return adcout
+
+def main():
+    while True:
+        inputVal0 = readadc(0, SPICLK, SPIMOSI, SPIMISO, SPICS)
+        print(inputVal0)
+
+if __name__=="__main__":
+    main()
