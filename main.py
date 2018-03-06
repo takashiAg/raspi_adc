2858acb44729ef8b5a9f0094b5dba25b3ca6efc2
安藤亮介 <crawd4274@gmail.com>
2018-03-06 14:37:17 +0900

    test

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import time
 def main():
     Adc = mcp3208()
     Start_time = time.time()
+    f = open('text.txt', 'w')
 
     Time_sample = 0.020
     i=1
@@ -14,26 +15,11 @@ def main():
         times = time.time()
         if Start_time < times + i:
             V = Adc.read(0)
-            if abs((i) % Time_sample) <0.01:
-                print(times, V)
+            f.writelines(times)
             i += Time_sample
 
-    """
-    while Start_time+10>time.time():
-        inputVal0 = Adc.read(0)
-        print(time.time(),inputVal0)
-    print(time.time()-Start_time)
 
-    N=100
-    Start_time=time.time()
-    for i in range(N):
-        inputVal0 = Adc.read_all()
-        print(inputVal0)
-    Stop_time=time.time()
-
-    print(Stop_time-Start_time)
-    """
-
+    f.close()
 
 if __name__ == "__main__":
     main()
