b6821f18d01df97448607ff1f0d01805c0292f76
安藤亮介 <crawd4274@gmail.com>
2018-03-06 13:06:39 +0900

    classing

--- main.py
+++ main.py
@@ -1,48 +1,11 @@
 # -*- coding: utf-8 -*-
+from mcp3208 import mcp3208
 
-from RPi import GPIO
-GPIO.setmode(GPIO.BCM)
-
-SPICLK, SPIMOSI, SPIMISO, SPICS=11,10,9,8
-GPIO.setup(SPICLK, GPIO.OUT)
-GPIO.setup(SPIMOSI, GPIO.OUT)
-GPIO.setup(SPIMISO, GPIO.IN)
-GPIO.setup(SPICS, GPIO.OUT)
-
-# MCP3208からSPI通信で12ビットのデジタル値を取得。0から7の8チャンネル使用可
-def readadc(adcnum, clockpin, mosipin, misopin, cspin):
-    if adcnum > 7 or adcnum < 0:
-        return -1
-    GPIO.output(cspin, GPIO.HIGH)
-    GPIO.output(clockpin, GPIO.LOW)
-    GPIO.output(cspin, GPIO.LOW)
-
-    commandout = adcnum
-    commandout |= 0x18  # スタートビット＋シングルエンドビット
-    commandout <<= 3  # LSBから8ビット目を送信するようにする
-    for i in range(5):
-        # LSBから数えて8ビット目から4ビット目までを送信
-        if commandout & 0x80:
-            GPIO.output(mosipin, GPIO.HIGH)
-        else:
-            GPIO.output(mosipin, GPIO.LOW)
-        commandout <<= 1
-        GPIO.output(clockpin, GPIO.HIGH)
-        GPIO.output(clockpin, GPIO.LOW)
-    adcout = 0
-    # 13ビット読む（ヌルビット＋12ビットデータ）
-    for i in range(13):
-        GPIO.output(clockpin, GPIO.HIGH)
-        GPIO.output(clockpin, GPIO.LOW)
-        adcout <<= 1
-        if i > 0 and GPIO.input(misopin) == GPIO.HIGH:
-            adcout |= 0x1
-    GPIO.output(cspin, GPIO.HIGH)
-    return adcout
 
 def main():
+    Adc=mcp3208()
     while True:
-        inputVal0 = readadc(0, SPICLK, SPIMOSI, SPIMISO, SPICS)
+        inputVal0 = Adc.read(0)
         print(inputVal0)
 
 if __name__=="__main__":
---
+++ mcp3208.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from RPi import GPIO
+
+
+class mcp3208():
+    def __init__(self, SPICLK=11, SPIMOSI=10, SPIMISO=9, SPICS=8):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(SPICLK, GPIO.OUT)
+        GPIO.setup(SPIMOSI, GPIO.OUT)
+        GPIO.setup(SPIMISO, GPIO.IN)
+        GPIO.setup(SPICS, GPIO.OUT)
+        self.cs = SPICS
+        self.ck = SPICLK
+        self.mosi = SPIMOSI
+        self.miso = SPIMISO
+
+    def read(self, adcnum):
+        if adcnum > 7 or adcnum < 0:
+            return -1
+        GPIO.output(self.cs, GPIO.HIGH)
+        GPIO.output(self.ck, GPIO.LOW)
+        GPIO.output(self.cs, GPIO.LOW)
+
+        commandout = adcnum
+        commandout |= 0x18
+        commandout <<= 3
+        for i in range(5):
+            if commandout & 0x80:
+                GPIO.output(self.mosi, GPIO.HIGH)
+            else:
+                GPIO.output(self.mosi, GPIO.LOW)
+            commandout <<= 1
+            GPIO.output(self.ck, GPIO.HIGH)
+            GPIO.output(self.ck, GPIO.LOW)
+        adcout = 0
+        # 13ビット読む（ヌルビット＋12ビットデータ）
+        for i in range(13):
+            GPIO.output(self.ck, GPIO.HIGH)
+            GPIO.output(self.ck, GPIO.LOW)
+            adcout <<= 1
+            if i > 0 and GPIO.input(self.miso) == GPIO.HIGH:
+                adcout |= 0x1
+        GPIO.output(self.cs, GPIO.HIGH)
+        return adcout
