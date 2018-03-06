79d31e285c61372b13febc908c35893d91ba9c65
安藤亮介 <crawd4274@gmail.com>
2018-03-06 15:01:34 +0900

    added save

--- main.py
+++ main.py
@@ -9,13 +9,29 @@ def main():
     Start_time = time.time()
     f = open('text.txt', 'w')
 
-    Time_sample = 0.100
+    Time_sample = 0.020
     i=1
     while True:
         times = time.time()
         if Start_time < times - i:
-            V = Adc.read(0)
+            V = Adc.read_all()
             f.write(str(times))
+            f.write(",")
+            f.write(str(V[0]))
+            f.write(",")
+            f.write(str(V[1]))
+            f.write(",")
+            f.write(str(V[2]))
+            f.write(",")
+            f.write(str(V[3]))
+            f.write(",")
+            f.write(str(V[4]))
+            f.write(",")
+            f.write(str(V[5]))
+            f.write(",")
+            f.write(str(V[6]))
+            f.write(",")
+            f.write(str(V[7]))
             f.write("\n")
             i += Time_sample
 
