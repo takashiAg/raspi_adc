03dd1ff7ef0f2738e3415ca543141fc963013702
安藤亮介 <crawd4274@gmail.com>
2018-03-10 17:30:18 +0900

    グラフ化 無し

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
-from Realtime_plot import realtime
-from sample_thread import sample_thread
+import mcp3208
 import time
+import file_write
 
 
 def main():
     print("program started")
 
-    Realtime = realtime(Quantity_data=8)
-    Sample_thread = sample_thread(Realtime)
-    Sample_thread.start()
+    Adc = mcp3208.mcp3208()
 
-    time.sleep(5)
-
+    file = file_write.file_write()
+    Start_time = time.time()
+    Time_sample = 0.020
+    Data_number = 0
     while True:
-        Realtime.pause()
+        Mesurement_time = time.time() - Start_time
+        if Mesurement_time > Data_number * Time_sample:
+            V = Adc.read_all()
+            file.write(Mesurement_time, V)
+            Data_number += 1
 
 
 if __name__ == "__main__":
--- sample_thread.py
+++ sample_thread.py
@@ -1,8 +1,5 @@
 import threading
-import mcp3208
-import time
-import file_write
-from make_axis import axis, axis_array
+
 
 
 class sample_thread(threading.Thread):
@@ -13,19 +10,4 @@ class sample_thread(threading.Thread):
         self.Realtime=Realtime
 
     def run(self):
-        Adc = mcp3208.mcp3208()
 
-        file = file_write.file_write()
-        Start_time = time.time()
-        x = axis()
-        y = axis_array(Array_length=8)
-        Time_sample = 0.020
-        Data_number = 0
-        while True:
-            Mesurement_time = time.time() - Start_time
-            if Mesurement_time > Data_number * Time_sample:
-                V = Adc.read_all()
-                file.write(Mesurement_time, V)
-                x.update(Mesurement_time)
-                y.update(V)
-                self.Realtime.plot(x.axis, y.axis)
