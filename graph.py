341c83a9001a616f63b6bbdaf205c2ca996732dd
安藤亮介 <crawd4274@gmail.com>
2018-03-10 18:29:06 +0900

    graphのテスト

--- graph.py
+++ graph.py
@@ -9,7 +9,7 @@ import os
 def current_file_path():
     base = os.path.dirname(os.path.abspath(__file__))
     current_file = os.path.normpath(os.path.join(base, "data", "current_file"))
-    f = open(current_file, 'w')
+    f = open(current_file, 'r')
     Data_file = f.readline()
     f.close()
     return Data_file
@@ -18,6 +18,7 @@ def current_file_path():
 def main():
     file_path = current_file_path()
 
+    print(file_path)
     Time_sample = 0.020
     Data_number = 0
     x = make_axis.axis(sample=100)
