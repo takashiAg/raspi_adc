dbe19a8b1944e6e19bc5b31c0ccb920955647054
安藤亮介 <crawd4274@gmail.com>
2018-03-10 16:32:43 +0900

    関数の中みミスってた

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ Time_before = 0
 
 def runner():
     global Time_before
-    Sample_thread = threading.Timer(Time_sample, target=runner)
+    Sample_thread = threading.Timer(Time_sample, runner)
     Sample_thread.start()
 
     Mesurement_time = time.time() - Time_before
