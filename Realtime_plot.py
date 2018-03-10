7041e322abc225131ad13793fbbd3eaf447725c3
安藤亮介 <crawd4274@gmail.com>
2018-03-10 15:09:32 +0900

    グラフが変なところを直したい

--- Realtime_plot.py
+++ Realtime_plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 class realtime():
     def __init__(self, x_data=[0], y_data=[0], Quantity_data=1):
         self.plt = plt
@@ -14,22 +15,23 @@ class realtime():
             self.lines.append(self.line)
 
     def plot(self, x_data, y_data):
-        y_data=np.array(y_data)
-        x_data=np.array(x_data)
-        if y_data.size==1:
-            y_data=[y_data]
+        y_data = np.array(y_data)
+        x_data = np.array(x_data)
+        if y_data.size == 1:
+            y_data = [y_data]
 
         y_min = y_data[0].min()
         y_max = y_data[0].max()
 
         for i in range(self.Quantity_data):
             self.lines[i].set_data(x_data, y_data[i])
-            y_min = min([y_data[i].min(), y_min])
-            y_max = max([y_data[i].max(), y_max])
+            if y_data[i].min() < y_min:
+                y_min = y_data[i].min()
+            if y_data[i].max() > y_max:
+                y_max = y_data[i].max()
 
         self.ax.set_xlim((x_data.min(), x_data.max()))
         self.ax.set_ylim((y_min, y_max))
 
     def pause(self):
         self.plt.pause(.01)
-
