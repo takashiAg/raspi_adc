8ba87325ccc84bda2838a43ac1f70c38bd620048
安藤亮介 <crawd4274@gmail.com>
2018-03-10 18:44:34 +0900

    テスト ファイル読み込み

--- graph.py
+++ graph.py
@@ -18,14 +18,14 @@ def current_file_path():
 
 def main():
     file_path = current_file_path()
-
-    f = open(file_path, "r")
     Time_sample = 0.020
     Data_number = 0
     x = make_axis.axis(sample=100)
     y = make_axis.axis_array(sample=100)
     while True:
+        f = open(file_path, "r")
         lines = [line for line in csv.reader(f)]
+        f.close()
         print(lines[-100:])
         time.sleep(1)
 
