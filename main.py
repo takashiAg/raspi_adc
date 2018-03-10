db861b684e5f86daaa100c8e700d644730105605
安藤亮介 <crawd4274@gmail.com>
2018-03-10 16:18:56 +0900

    OK

--- main.py
+++ main.py
@@ -11,22 +11,26 @@ import file_write
 x = axis()
 y = axis_array(Array_length=8)
 
+Adc = mcp3208.mcp3208(Voltage_divider=5)
+
+file = file_write.file_write()
+Start_time = time.time()
+Time_sample = 0.020
+Data_number = 0
+
+Time_before = 0
+
 
 def runner():
-    Adc = mcp3208.mcp3208(Voltage_divider=5)
+    Sample_thread = threading.Timer(Time_sample, target=runner)
+    Sample_thread.start()
 
-    file = file_write.file_write()
-    Start_time = time.time()
-    Time_sample = 0.040
-    Data_number = 0
-    while True:
-        Mesurement_time = time.time() - Start_time
-        if Mesurement_time > Data_number * Time_sample:
-            V = Adc.read_all()
-            file.write(Mesurement_time, V)
-            x.update(Mesurement_time)
-            y.update(V)
-            Data_number += 1
+    Mesurement_time = time.time() - Time_before
+    V = Adc.read_all()
+    file.write(Mesurement_time, V)
+    x.update(Mesurement_time)
+    y.update(V)
+    Time_before = time.time()
 
 
 def main():
@@ -35,7 +39,7 @@ def main():
     Realtime = realtime(Quantity_data=8)
     # Sample_thread = sample_thread(x, y)
     # Sample_thread.start()
-    Sample_thread = threading.Thread(target=runner, name="Sample_thread")
+    Sample_thread = threading.Thread(target=runner)
     Sample_thread.start()
 
     time.sleep(5)
